ebf97b54d4bcd1e2f32e3dc069461b38c929c648
Chris Uzelac <cuzelac@gmail.com>
2021-02-07 19:42:37 -0800

    Update zoom-mute.ino
    use `NKROKeyboard` and configure buttons using arrays

--- zoom-mute.ino
+++ zoom-mute.ino
@@ -5,30 +5,43 @@
 const byte BUTTON_PIN = 7;
 const Button btn(BUTTON_PIN);
 
+const KeyboardKeycode ZOOM_MAC_MUTE_ALL[] = {
+  KEY_LEFT_GUI,
+  KEY_LEFT_CTRL,
+  KEY_M,
+};
+
+const KeyboardKeycode ZOOM_WIN_MUTE_ALL[] = {
+  KEY_LEFT_ALT,
+  KEY_M,
+};
+
 void setup() {
   Serial.begin(9600);
-  
+
   btn.begin();
-  Keyboard.begin();
+  NKROKeyboard.begin();
 }
 
 void loop() {
   btn.read();
-  if(btn.wasPressed()) {
-    Serial.println("WRITE!");
-
-    zoomMuteAllMac();
+  if (btn.wasPressed()) {
+    auto& keys = ZOOM_MAC_MUTE_ALL;
+    
+    doKeypress(keys, sizeof(keys)/sizeof(keys[0]));
   }
 }
 
-// See key layouts here: https://github.com/NicoHood/HID/blob/master/src/KeyboardLayouts/ImprovedKeylayouts.h
-void zoomMuteAllMac() {
-  Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.write(KEY_M);
-  Keyboard.release(KEY_LEFT_CTRL);
-  Keyboard.release(KEY_LEFT_GUI);
+void doKeypress(KeyboardKeycode keys[], int size){
+  for (int i=0; i<size; ++i){
+    NKROKeyboard.add(keys[i]);
+  }
+  NKROKeyboard.send();
+  NKROKeyboard.releaseAll();
+  NKROKeyboard.println();
 
   delay(100);
   Keyboard.write(KEY_RETURN);
 }
+
+// See key layouts here: https://github.com/NicoHood/HID/blob/master/src/KeyboardLayouts/ImprovedKeylayouts.h
